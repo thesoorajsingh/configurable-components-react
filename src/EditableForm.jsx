23d5852d20c4f093deb9328a57769a06db758b18
Sooraj Singh <soorajsingh.work@gmail.com>
2022-12-22 07:30:05 +0000

    Update EditableForm.jsx and styles.css

--- src/EditableForm.jsx
+++ src/EditableForm.jsx
@@ -4,21 +4,24 @@ import "./styles.css";
 const EditableForm = ({ config }) => {
   const { formMeta, formInputs, formConfig } = config;
   const [formData, setFormData] = useState({});
+    const [wasFormEdited, setWasFormEdited] = useState(false)
   
   const InputField = ({formMeta, formData, formInputs, item, index}) => {
-    const [editable, setEditable] = useState(false)
+    const [editable, setEditable] = useState(true)
     return <div className="inputField">
               <p>{item}</p>
               <input
                 className="input"
                 value={formData[item]}
                 disabled={editable}
-                onChange={(e) =>
-                  setFormData(...formData, `${item}: e.target.value`)
-                }
+                onChange={(e) =>{
+                  setFormData(...formData, `${item}: ${e.target.value}`);
+                  setWasFormEdited(true);
+                  console.log(wasFormEdited)
+                }}
                 {...formInputs[item]}
               />
-              <i className='fa-solid fa-pencil' onClick={()=>{setEditable(!editable)}} />
+              <i className='fa-solid fa-pencil' style={{cursor: 'pointer'}} onClick={()=>{setEditable(!editable)}} />
             </div>
   }
   
@@ -30,7 +33,9 @@ const EditableForm = ({ config }) => {
             <InputField formData={formData} formInputs={formInputs} formMeta={formMeta} item={item} index={index}/>
           );
         })}
+        {wasFormEdited && 
         <button type="submit">Submit</button>
+        }
       </form>
     </div>
   );
